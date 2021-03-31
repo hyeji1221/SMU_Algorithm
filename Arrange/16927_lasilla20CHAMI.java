ee6a3cbc7fbefec420ab805d4b26543c275c3030
JEONG CHA MI <lasilla20@naver.com>
2021-04-01 01:46:54 +0900

    Update 16927_lasilla20CHAMI.java

--- Arrange/16927_lasilla20CHAMI.java
+++ Arrange/16927_lasilla20CHAMI.java
@@ -1,15 +1,14 @@
 import java.util.*;
-
-public class Arr{
+public class Main {
 
 	static int N;
 	static int M;
 	static int rotate;
 	static int[][] array;
-  int rowS, rowE, colS, colE;
-  
-  public void Input(){
-  	Scanner sc = new Scanner(System.in);
+	
+    public static void Input(){
+        //입력 받기
+        Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 		M = sc.nextInt();
 		rotate = sc.nextInt();
@@ -20,54 +19,65 @@ public class Arr{
 				array[i][j] = sc.nextInt();
 			}
 		}
-  }
-	
-	public static void main(String[] args) {
-
-		Input();
-		rowS = colS = 0;
-		rowE = N-1;
-		colE = M-1;
+    }
     
-		while(true) {
-			int size = (rowE-rowS+1)*2 + (colE-colS+1)*2 -4;
-			change(rowS,rowE,colS,colE,rotate%size);
-			rowS += 1;
-			rowE -= 1;
-			colS += 1;
-			colE -= 1;
-			if(rowS>rowE || colS>colE) break;
-		}
-		
-	public static void change(int rowStart, int rowEnd, int colStart, int colEnd, int cnt) {
-		for(int k=0;k<cnt;k++) {
-      //임시로 담아두기
-			int temp = array[rowStart][colStart];
-
-			for(int j=colStart;j<colEnd;j++) {
-				array[rowStart][j] = array[rowStart][j+1]; 
-			}//왼쪽 이동
-			
-			for(int i=rowStart;i<rowEnd;i++) {
-				array[i][colEnd] = array[i+1][colEnd];
-			}//위로 이동
-			
-			for(int j=colEnd;j>colStart;j--) {
-				array[rowEnd][j] = array[rowEnd][j-1];
-			}//오른쪽 이동
-			
-			for(int i=rowEnd;i>rowStart;i--) {
-				array[i][colStart] = array[i-1][colStart];
-			}//위로 이동
-			array[rowStart+1][colStart] = temp;
-		}
-	}
-    
-  		for(int i=0;i<N;i++) {
+    public static void prt(){
+        //출력 하기
+        for(int i=0;i<N;i++) {
 			for(int j=0;j<M;j++) {
 				System.out.print(array[i][j]+" ");
 			}
 			System.out.println();
 		}
+    }
+    
+	public static void main(String[] args) {
+		Input();
+        //각 행, 열 범위 지정
+		int rowS = 0;
+		int rowE = N-1;
+		int colS = 0;
+		int colE = M-1;
+
+		while(true) {
+			int size = (rowE-rowS+1)*2 + (colE-colS+1)*2 -4;
+            //rotate%size만큼 돌림(최소화 하기)
+			change(rowS,rowE,colS,colE,rotate%size);
+            //start와 end를 한 칸씩 이동
+			rowS+=1;
+			rowE-=1;
+			colS+=1;
+			colE-=1;
+            //만약 start지점이 end를 넘으면 종료
+			if(rowS>rowE || colS>colE) break;
+		}
+        prt();
 	}
+	
+	public static void change(int rowS, int rowE, int colS, int colE, int cnt) {
+        //cnt만큼 반복
+		for(int k=0;k<cnt;k++) {
+            //임시로 저장
+			int temp = array[rowS][colS];
+            
+			for(int j=colS;j<colE;j++) {
+				array[rowS][j] = array[rowS][j+1]; 
+			}//왼쪽 이동
+			
+			for(int i=rowS;i<rowE;i++) {
+				array[i][colE] = array[i+1][colE];
+			}//위로 이동
+			
+			for(int j=colE;j>colS;j--) {
+				array[rowE][j] = array[rowE][j-1];
+			}//오른쪽 이동
+			
+			for(int i=rowE;i>rowS;i--) {
+				array[i][colS] = array[i-1][colS];
+			}//아래 이동
+            		//저장했던 것 옮김
+			array[rowS+1][colS] = temp;
+		}
+	}
+
 }
