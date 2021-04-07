6e06006618bb5fb996a924ef50487588fcc37b9d
Hyeji Lim <59350891+hyeji1221@users.noreply.github.com>
2021-04-08 01:55:01 +0900

    Update 3190_hyeji1221.cpp

--- LinkedList/3190_hyeji1221.cpp
+++ LinkedList/3190_hyeji1221.cpp
@@ -16,7 +16,6 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    //freopen("../input.txt", "r", stdin);
 
     int size, apple;
     cin >> size >> apple;
