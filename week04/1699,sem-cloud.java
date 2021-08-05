25fb1d47acc7b36141924a328fbe19819c26081d
Semi kang <71436576+semi-cloud@users.noreply.github.com>
2021-08-05 22:23:43 +0900

    Create 1699,sem-cloud.java

---
+++ week04/1699,sem-cloud.java
@@ -0,0 +1,25 @@
+public class Main {
+	
+	static int N;  
+	static int[] dp; 
+	static int cnt;
+  static int max;
+    
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		dp = new int[N+1];  
+		 
+		for(int i = 1 ; i <= N ; i++) {
+			dp[i] = i;           
+			for(int j = 1; j*j <=i ;j++) {
+				if(dp[i] > dp[i-j*j] + 1) {    
+					dp[i] = dp[i-j*j] + 1;
+				}
+			}
+		}
+
+		System.out.println(dp[N]);
+	}
+}
