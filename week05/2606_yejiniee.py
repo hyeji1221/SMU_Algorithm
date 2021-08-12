0c7cb56f25d1879d3850bba7bfa5ba3eda576545
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2021-08-12 17:46:54 +0900

    Update 2606_yejiniee.py

--- week05/2606_yejiniee.py
+++ week05/2606_yejiniee.py
@@ -3,7 +3,7 @@ n=int(sys.stdin.readline()) #컴퓨터의 수
 c=int(sys.stdin.readline()) #컴퓨터 쌍의 수
 
 graph=[[]*n for _ in range(n+1)]
-for _ in range(c): #그래프 구현
+for _ in range(c): #인접리스트로 그래프 구현
     a,b=map(int,sys.stdin.readline().split())
     graph[a].append(b)
     graph[b].append(a)
