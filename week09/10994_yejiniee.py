b415d7d9f3af97a7e2728a3e585eccc3fa004e94
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2021-09-19 18:22:58 +0900

    Update 10994_yejiniee.py

--- week09/10994_yejiniee.py
+++ week09/10994_yejiniee.py
@@ -7,11 +7,11 @@ def star_draw(n, idx):
     l=4*n-3
     
     for i in range(idx, l+idx):
-        star[idx][i]='*' #위
-        star[idx+l-1][i]='*' #아래
+        star[idx][i]='*' #맨위
+        star[idx+l-1][i]='*' #맨아래
         
-        star[i][idx]='*' #왼쪽
-        star[i][idx+l-1]='*' #오른쪽
+        star[i][idx]='*' #맨왼쪽
+        star[i][idx+l-1]='*' #맨오른쪽
 
     return star_draw(n-1, idx+2) #재귀함수
     
